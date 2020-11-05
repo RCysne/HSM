b6eeaa6f92a4b261987ef3d09816400bb0a45429
Ronaldo Cysne <ronaldocysne@gmail.com>
2020-11-04 21:40:23 -0300

    Exercício IMC
    Crie um programa que solicite os dados a cada um dos alunos e mostre:
    a) o IMC (Índice de Massa Corporal) de cada estudante;
    b) o estudante com maior IMC;
    c) o estudante com menor IMC;
    d) a altura média dos estudantes.
    
    Além disso, o programa deve emitir um alerta para aqueles que fazem parte das classificações magreza e obesidade grau I, II ou III.
    
    O IMC é calculado pela fórmula peso/altura² e as classificações são:
    
    CLASSIFICAÇÃO 1
    IMC: menor que 18,5
    Classificação: Magreza
    Obesidade: Grau 0
    
    CLASSIFICAÇÃO 2
    IMC: entre 18,5 e 24,9
    Classificação: Normal
    Obesidade: Grau 0
    
    CLASSIFICAÇÃO 3
    IMC: entre 25,0 e 29,9
    Classificação: Sobrepeso
    Obesidade: Grau I
    
    CLASSIFICAÇÃO 4
    IMC: entre 30,0 e 39,9
    Classificação: Obesidade
    Obesidade: Grau II
    
    CLASSIFICAÇÃO 1
    IMC: maior que 40,0
    Classificação: Obesidade grave
    Obesidade: Grau III

--- IMC/academia.js
+++ IMC/academia.js
@@ -1,18 +1,22 @@
-const btn = document.querySelector('#btn').addEventListener('click', calc);
+const nome = prompt('Digite o seu nome: ');
+const idade = prompt('Digite a sua idade: ');
+const altura = prompt('Digite a sua altura: ');
+const peso = prompt('Digite o seu peso: ');
 
-// Adicionar valor do input ao objeto
-function calc() {
-  let name = document.querySelector('#name').value;
-  let age = Number(document.querySelector('#age').value);
-  let height = Number(document.querySelector('#height').value);
-  let pound = Number(document.querySelector('#pound').value);
+const imc = (peso / (altura * altura)).toFixed(2);
 
-  let aluno = {
-    nome: name,
-    idade: age,
-    altura: height,
-    peso: pound,
-  };
-
-  return aluno;
+if (imc < 18.5) {
+  alert('Seu IMC é: ' + imc);
+  alert('Atenção! Você está na linha da Magreza.');
+} else if (imc >= 18.5 && imc < 25) {
+  alert('Seu IMC é: ' + imc);
+} else if (imc > 25 && imc < 30) {
+  alert('Seu IMC é: ' + imc);
+  alert('Atenção! Você está com sobrepeso. Obesidade grau I');
+} else if (imc >= 30 && imc < 40) {
+  alert('Seu IMC é: ' + imc);
+  alert('Atenção! Você está com Obesidade grau II.');
+} else if (imc >= 40) {
+  alert('Seu IMC é: ' + imc);
+  alert('Atenção! Você está com Obesidade grau III.');
 }
